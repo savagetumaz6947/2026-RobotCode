f1a47fb2e0797c5a0078dd1a39ea264a2e814a39
ytgtimothy <timothyger08@gmail.com>
2026-02-13 11:52:24 +0800

    Auto Update 2

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems;
 
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,6 +8,7 @@ import frc.robot.subsystems.conveyor.ConveyorSubsystem;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.intakepivot.IntakePivotSubsystem;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
+import org.littletonrobotics.junction.Logger;
 
 public class Superstructure extends SubsystemBase {
 
@@ -96,19 +95,18 @@ public class Superstructure extends SubsystemBase {
             conveyor.goToShooter())
         .withName("Superstructure_Shoot");
   }
+
   public Command prepareShootAuto() {
-    return prepareShoot().withTimeout(0.25).withName("Superstructure_PrepareShootAuto");
+    return prepareShoot().withTimeout(0.5).withName("Superstructure_PrepareShootAuto");
   }
+
   public Command shootAuto() {
     return Commands.sequence(
             Commands.runOnce(() -> setState(SuperstructureState.SHOOT)),
-            Commands.waitUntil(shooter::readyForHub).withTimeout(1.25),
-            conveyor.goToShooter().withTimeout(0.75),
-            Commands.parallel(
-                conveyor.stop(),
-                shooter.stopShooter()
-            ))
-        .withName("Superstructure_ShootAuto");       
+            Commands.waitUntil(shooter::readyForHub).withTimeout(0.75),
+            conveyor.goToShooter().withTimeout(1),
+            Commands.parallel(conveyor.stop(), shooter.stopShooter()))
+        .withName("Superstructure_ShootAuto");
   }
 
   public Command eject() {
